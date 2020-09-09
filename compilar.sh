abd80ce176527898b48358b7547486e51a72dea1
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2020-09-09 17:57:03 -0300

    Removendo arquivos desnecessários

--- compilar.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-pdflatex --jobname=livro main.tex
-[[ $? ]] || pdflatex --jobname=livro main.tex
--- limpar.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-rm *.aux **/**/*.aux *.log *.out *.fdb_latexmk *.fls *.gz paginas_png/* *.toc
